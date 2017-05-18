62bb62b79452d7b9b3b5b11614d80f42fb1d5046
IVLCGJC <iv.lcgjc@gmail.com>
2017-05-18 10:51:40 +0200

    Removed ViewPager... Could not make to work with onClickListener

--- app/src/main/java/com/example/android/musicplayerstructure/LibraryFragment.java
+++ app/src/main/java/com/example/android/musicplayerstructure/LibraryFragment.java
@@ -1,16 +1,54 @@
 package com.example.android.musicplayerstructure;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class LibraryFragment extends Fragment {
+public class LibraryFragment extends AppCompatActivity {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_library_fragment, container, false);
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library_fragment);
+        //Find the playlist image and set an OnClickListener to it.
+        ImageView playlist = (ImageView) findViewById(R.id.playlist);
 
+        if (playlist != null) {
+            playlist.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Intent playlistIntent = new Intent(LibraryFragment.this, PlaylistFragment.class);
+                    startActivity(playlistIntent);
+                }
+            });
+        }
+        //Find the library image and set an OnClickListener to it.
+        ImageView library = (ImageView) findViewById(R.id.library);
+
+        if (library != null) {
+            library.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent libraryIntent = new Intent(LibraryFragment.this, LibraryFragment.class);
+                    startActivity(libraryIntent);
+                }
+            });
+        }
+        //Find the search image and set an OnClickListener to it.
+        ImageView search = (ImageView) findViewById(R.id.search);
+
+        if (search != null) {
+            search.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent searchIntent = new Intent(LibraryFragment.this, SearchFragment.class);
+                    startActivity(searchIntent);
+                }
+            });
+        }
+    }
 }
--- app/src/main/java/com/example/android/musicplayerstructure/PlaylistFragment.java
+++ app/src/main/java/com/example/android/musicplayerstructure/PlaylistFragment.java
@@ -1,15 +1,57 @@
 package com.example.android.musicplayerstructure;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class PlaylistFragment extends Fragment {
+public class PlaylistFragment extends AppCompatActivity {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_playlist_fragment, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist_fragment);
+        //Find the playlist image and set an OnClickListener to it.
+        ImageView playlist = (ImageView) findViewById(R.id.playlist);
+
+        if (playlist != null) {
+            playlist.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(PlaylistFragment.this, "Clicked", Toast.LENGTH_SHORT).show();
+                    Intent playlistIntent = new Intent(PlaylistFragment.this, PlaylistFragment.class);
+                    startActivity(playlistIntent);
+                }
+            });
+        }
+        //Find the library image and set an OnClickListener to it.
+        ImageView library = (ImageView) findViewById(R.id.library);
+
+        if (library != null) {
+            library.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(PlaylistFragment.this, "Clicked", Toast.LENGTH_SHORT).show();
+                    Intent libraryIntent = new Intent(PlaylistFragment.this, LibraryFragment.class);
+                    startActivity(libraryIntent);
+                }
+            });
+        }
+        //Find the search image and set an OnClickListener to it.
+        ImageView search = (ImageView) findViewById(R.id.search);
+
+        if (search != null) {
+            search.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(PlaylistFragment.this, "Clicked", Toast.LENGTH_SHORT).show();
+                    Intent searchIntent = new Intent(PlaylistFragment.this, SearchFragment.class);
+                    startActivity(searchIntent);
+                }
+            });
+        }
+
     }
 }
